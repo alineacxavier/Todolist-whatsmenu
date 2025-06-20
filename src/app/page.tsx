ce6b1716512b969b95dce49cbc10a949ac9eb7fb
Aline Azevedo <aline-azevedo2010@hotmail.com>
2025-06-20 13:26:41 -0300

    primeiro commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,114 @@
-import Image from "next/image";
+"use client"; // Para rodar no navegador
+
+import { useEffect, useState } from "react"; // gerenciar estado e efeitos
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  // Criar os componentes da página inicial(e unica) hehe
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [tarefa, setTask] = useState(""); // useState é usado para criar tarefas enquanto (aqui vamos guardar o texto do input)
+
+  type Tarefa = {
+    // definindo o tipo Tarefa
+    texto: string;
+    feita: boolean;
+    editando?: boolean; //
+  };
+
+  const [tarefas, setTasks] = useState<Tarefa[]>([]); // useState é usado para criar uma lista de tarefas ( objetos com texto e status de conclusão)
+
+  // tarefas salvas
+  useEffect(() => {
+    const tarefasSalvas = localStorage.getItem("tarefas");
+    if (tarefasSalvas) {
+      setTasks(JSON.parse(tarefasSalvas));
+    }
+  }, []);
+
+  // Salva tarefas sempre 
+  useEffect(() => {
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  }, [tarefas]);
+
+  function handleAddTask() {
+    // Função para adicionar uma nova tarefa
+    setTasks([...tarefas, { texto: tarefa, feita: false }]);
+    setTask(""); // limpa o campo
+  }
+
+  function alternarTarefa(index: number) {
+    const novasTarefas = [...tarefas];
+    novasTarefas[index].feita = !novasTarefas[index].feita;
+    setTasks(novasTarefas);
+  }
+  function ativarEdicao(index: number) {
+    const novasTarefas = [...tarefas];
+    novasTarefas[index].editando = true; // Marca a tarefa como editando
+    setTasks(novasTarefas);
+  }
+  function salvarEdicao(index: number, novoTexto: string) {
+    const novasTarefas = [...tarefas];
+    novasTarefas[index].texto = novoTexto;
+    novasTarefas[index].editando = false;
+    setTasks(novasTarefas);
+  }
+  function deletarTarefa(index: number) {
+    const novasTarefas = tarefas.filter((_, i) => i !== index); // filtra as tarefas para remover
+    setTasks(novasTarefas);
+  }
+
+  return (
+    <main>
+      <h1>Todo-List Whastmenu</h1>
+      <input
+        type="text"
+        placeholder="O que eu preciso fazer?"
+        value={tarefa} // O valor do input é controlado pelo estado tarefa
+        onChange={(e) => setTask(e.target.value)} // Atualiza o estado da tarefa quando o input muda
+      />
+      <button onClick={handleAddTask}>Adicionar</button>{" "}
+      {/* Botão para adicionar a tarefa */}
+      <ul>
+        {tarefas.map(
+          (
+            t,
+            index // Mapeia as tarefas e cria um item de lista para cada uma
+          ) => (
+            <li key={index}>
+              <input
+                type="checkbox" // Checkbox para marcar a tarefa como feita
+                checked={t.feita}
+                onChange={() => alternarTarefa(index)}
+              />
+
+              {t.editando ? (
+                <input
+                  type="text"
+                  defaultValue={t.texto}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      salvarEdicao(index, (e.target as HTMLInputElement).value); // Salva a edição quando apertar Enter
+                    }
+                  }}
+                />
+              ) : (
+                <span
+                  className={t.feita ? "feita" : ""}
+                  onDoubleClick={() => ativarEdicao(index)}
+                  style={{ marginLeft: "8px", cursor: "pointer", flex: 1 }} // edita a tarefa ao dar um duplo clique
+                >
+                  {t.texto}
+                </span>
+              )}
+              <button
+                className="botao-excluir"
+                onClick={() => deletarTarefa(index)}
+              >
+                🗑️
+              </button>
+            </li>
+          )
+        )}
+      </ul>
+    </main>
   );
 }
